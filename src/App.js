62942a2ea0731535281e65da443eda5b71db7632
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-12-27 18:51:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import Login from './Login';
 import { BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 import {useStateValue} from './StateProvider';
+import {auth} from './firebase';
 
 function App() {
   const [{user}, dispatch] = useStateValue();
+ // bellow useEffect function forwhenever we refresh app
+ // it comes to app only not to login page.
+  useEffect(() =>{
+    auth.onAuthStateChanged(user=>{
+      dispatch({
+          type:"SET_USER",
+          user:user,
+        }
+      )
+
+    })
+  }, [])
   return (
     <div className="app">
         {!user ? (
